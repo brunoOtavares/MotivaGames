8fd5e6824404a1d30147879200a3b13ba5567e3d
brunoOtavares <matavaes58@gmail.com>
2025-12-22 11:23:01 -0300

    Arrumando tudo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,39 +1,10 @@
-import { useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
-import { auth } from '../../firebase';
+import Header from '../Header/Header';
 import './Home.css';
 
 function Home() {
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      navigate('/');
-    } catch (error) {
-      console.error('Erro ao fazer logout:', error);
-    }
-  };
-
   return (
     <div className="dashboard">
-      <header className="dashboard-header">
-        <img src="/vite.svg" alt="MotivaGames Logo" className="logo" onClick={() => navigate('/home')} />
-        <nav>
-          <button onClick={() => navigate('/home')} className="nav-button active">
-            Home
-          </button>
-          <button onClick={() => navigate('/perfil')} className="nav-button">
-            Perfil
-          </button>
-          <button onClick={() => navigate('/sobre')} className="nav-button">
-            Sobre
-          </button>
-        </nav>
-        <button onClick={handleLogout} className="nav-button logout">
-          Sair
-        </button>
-      </header>
+      <Header />
       <main className="dashboard-content">
         <h1>Home</h1>
       </main>
