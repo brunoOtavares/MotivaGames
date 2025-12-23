b3ecae63881fee252e399611ec5a203702ccde4c
brunoOtavares <matavaes58@gmail.com>
2025-12-23 07:33:13 -0300

    fix: Update

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,41 +2,9 @@ import { useNavigate } from 'react-router-dom';
 import './LandingPage.css';
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   return (
-<<<<<<< HEAD
-    <div className="landing-page">
-      <button className="login-button" onClick={() => navigate('/login')}>
-        Login
-      </button>
-      <div className="landing-container">
-    <header className="landing-header">
-      <div>
-      <h1>Bem-vindo ao MotivaGames!</h1>
-      <h2>Descubra, jogue e motive-se com nossa plataforma de jogos.</h2>
-    </div>
-    </header>
-    <main className="landing-main">
-      <section className="landing-features">
-        <h3>Recursos</h3>
-          <h4>Jogos motivacionais exclusivos</h4>
-          <img src="https://cdn.dribbble.com/userupload/42346351/file/original-3861e12764d32a1a5ad6e907674fe7a2.gif" id="firstgif" alt="jogos-motivacionais" />
-          <h4>Comunidade ativa</h4>
-          <img src="https://miro.medium.com/v2/1*Vq0sQ79QIZV6V1R-t7qtmw.gif" id="secondgif" alt="comunidade-ativa" />
-          <h4>Desafios diários</h4>
-          <img src="https://c.tenor.com/p322zJ_osRkAAAAC/tenor.gif" id="thirdgif" alt="desafios-diarios" />
-      </section>
-      <section className="landing-cta">
-        <button onClick={() => navigate('/login')} className="landing-btn">Entrar</button> 
-      </section>
-      <section className="landing-cta">        
-        <button onClick={() => navigate('/sobre')} className="landing-btn secondary">Saiba Mais</button>
-      </section>
-    </main>
-    <footer className="landing-footer">
-      <p>&copy; 2025 MotivaGames. Todos os direitos reservados.</p>
-    </footer>
-      </div>
-=======
     <div className="landing-container">
       <header className="landing-header">
         <h1>Bem-vindo ao MotivaGames!</h1>
@@ -70,11 +38,11 @@ function LandingPage() {
         </section>
 
         <section className="landing-cta">
-          <a href="/login" className="landing-btn">Entrar</a>
+          <button onClick={() => navigate('/login')} className="landing-btn">Entrar</button>
         </section>
 
         <section className="landing-cta">
-          <a href="/sobre" className="landing-btn secondary">Saiba Mais</a>
+          <button onClick={() => navigate('/sobre')} className="landing-btn secondary">Saiba Mais</button>
         </section>
       </main>
 
@@ -83,7 +51,6 @@ function LandingPage() {
           &copy; 2025 MotivaGames. Todos os direitos reservados.
         </p>
       </footer>
->>>>>>> 9fd9d5c89a07f0968fe981e3d66f89e1a77652b3
     </div>
   );
 }
