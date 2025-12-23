c5d521ca5e8f3feef9e6ee263a154ad147e7aaef
brunoOtavares <matavaes58@gmail.com>
2025-12-23 07:18:21 -0300

    update

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -9,11 +9,33 @@ function LandingPage() {
       <button className="login-button" onClick={() => navigate('/login')}>
         Login
       </button>
+      <div className="landing-container">
+    <header className="landing-header">
       <div>
-        <h1 className='olamundo'>
-          ola
-
-        </h1>
+      <h1>Bem-vindo ao MotivaGames!</h1>
+      <h2>Descubra, jogue e motive-se com nossa plataforma de jogos.</h2>
+    </div>
+    </header>
+    <main className="landing-main">
+      <section className="landing-features">
+        <h3>Recursos</h3>
+          <h4>Jogos motivacionais exclusivos</h4>
+          <img src="https://cdn.dribbble.com/userupload/42346351/file/original-3861e12764d32a1a5ad6e907674fe7a2.gif" id="firstgif" alt="jogos-motivacionais" />
+          <h4>Comunidade ativa</h4>
+          <img src="https://miro.medium.com/v2/1*Vq0sQ79QIZV6V1R-t7qtmw.gif" id="secondgif" alt="comunidade-ativa" />
+          <h4>Desafios diários</h4>
+          <img src="https://c.tenor.com/p322zJ_osRkAAAAC/tenor.gif" id="thirdgif" alt="desafios-diarios" />
+      </section>
+      <section className="landing-cta">
+        <button onClick={() => navigate('/login')} className="landing-btn">Entrar</button> 
+      </section>
+      <section className="landing-cta">        
+        <button onClick={() => navigate('/sobre')} className="landing-btn secondary">Saiba Mais</button>
+      </section>
+    </main>
+    <footer className="landing-footer">
+      <p>&copy; 2025 MotivaGames. Todos os direitos reservados.</p>
+    </footer>
       </div>
     </div>
   );
